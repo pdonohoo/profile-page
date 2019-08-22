8519b40390eed370425de944fba5789c7a4915cb
Preston Donohoo <preston.s.donohoo@gmail.com>
2019-08-22 10:19:01 -0600

    fixed mobile friendly

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,67 @@
-import React from "react"
+import React, {useState} from "react"
+import axios from 'axios'
+
+// const axios = require('axios')
+
 
 import Layout from "../components/layout"
 import contactStyles from "../styles/contact.module.scss"
 
 const Contact = () => {
+
+  // const [firstName, setfirstName] = useState('')
+  // const [lastName, setlastName] = useState('')
+  // const [phoneNumber, setphoneNumber] = useState('')
+  // const [email, setEmail] = useState('')
+  // const [message, setmessage] = useState('')
+
+
+  // const onSubmit = (e) => {
+  //   e.preventDefault()
+  //   alert('worked')
+  //   sendMessage()
+  // }
+
+  // const handleChange = (e) => {
+  //   setfirstName(e.target.name = e.target.value)
+  //   setlastName(e.target.name = e.target.value)
+  //   setphoneNumber(e.target.name = e.target.value)
+  //   setEmail(e.target.name = e.target.value)
+  //   setmessage(e.target.name = e.target.value)
+  //   console.log(setlastName)
+  // }
+
+  // const GOOGLE_FORM_FIRSTNAME_ID = 'entry.1751171160'
+  // const GOOGLE_FORM_LASTNAME_ID = 'entry.344642064'
+  // const GOOGLE_FORM_PHONENUMBER_ID = 'entry.646590667'
+  // const GOOGLE_FORM_EMAIL_ID = 'entry.1971860555'
+  // const GOOGLE_FORM_MESSAGE_ID = 'entry.1101212898'
+  // const GOOGLE_FORM_ACTION_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSfYb3KwcAUR4lbasB7Ay-SmGy09Wb1WFFqyvd74l4BWA4GH3w/formResponse'
+  // const CORS_PROXY = 'https://cors-escape.herokuapp.com/'
+
+  // const sendMessage = () => {
+
+  //   const formData = new formData()
+    
+  //   formData.append(GOOGLE_FORM_FIRSTNAME_ID, firstName)
+  //   formData.append(GOOGLE_FORM_LASTNAME_ID, lastName)
+  //   formData.append(GOOGLE_FORM_PHONENUMBER_ID, phoneNumber)
+  //   formData.append(GOOGLE_FORM_EMAIL_ID, email)
+  //   formData.append(GOOGLE_FORM_MESSAGE_ID, message)
+
+  //   axios.post(CORS_PROXY + GOOGLE_FORM_ACTION_URL, formData)
+  //   .then(() => {
+  //     setfirstName('')
+  //     setlastName('')
+  //     setphoneNumber('')
+  //     setEmail('')
+  //     setmessage('')
+  //   }).catch(() => {
+      
+  //   })
+  // }
+  
+
   return (
     <Layout>
       <div>
@@ -11,28 +69,32 @@ const Contact = () => {
           <h1>Preston Donohoo</h1>
           <h4>Send me a message!</h4>
         </div>
+          <div className={contactStyles.header}>
+          Email:<a href='mailto:preston.s.donohoo@gmail.com'>Preston Donohoo</a>
+
+          </div>
       </div>
-      <form className={contactStyles.form}>
+      {/* <form  className={contactStyles.form}>
         <div className={contactStyles.inputs}>
           <div>First Name </div>
           <div>Last Name</div>
         </div>
         <div className={contactStyles.inputs}>
-          <input className={contactStyles.input}></input>
-          <input className={contactStyles.input}></input>
+          <input onChange={handleChange} name='firstName' value={firstName} className={contactStyles.input}></input>
+          <input onChange={handleChange} name='lastName' value={lastName} className={contactStyles.input}></input>
         </div>
         <div className={contactStyles.inputs}>
-          <div>Phone #</div>
+          <div>Phone Number </div>
           <div>Email </div>
         </div>
         <div className={contactStyles.inputs}>
-          <input className={contactStyles.input}></input>
-          <input className={contactStyles.emailInput}></input>
+          <input onChange={handleChange} name='phoneNumber' value={phoneNumber}  className={contactStyles.input}></input>
+          <input onChange={handleChange} name='Email' value={email} className={contactStyles.emailInput}></input>
         </div>
-        message <textarea className={contactStyles.message}></textarea>
-        <button className={contactStyles.button}>send</button>
+        message <textarea onChange={handleChange} name='message' value={message} className={contactStyles.message}></textarea>
+        <button onChange={handleChange} onClick={onSubmit} className={contactStyles.button}>send</button>
         
-      </form>
+      </form> */}
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import { graphql, useStaticQuery } from 'gatsby'
 import BackgroundImage from 'gatsby-background-image'
 import Github from "../images/github.inline.svg"
 import Linkedin from '../images/linkedin.inline.svg'
+import indexStyles from '../styles/index.module.scss'
 
 
 
@@ -31,14 +32,14 @@ const Index = () => {
 
   return (
 
-    <BackgroundImage fluid={imageData} >
-      <Layout>
-        <div style={{ textAlign: 'center' }}>
+    <BackgroundImage className={indexStyles.background} fluid={imageData}  >
+      <Layout className={indexStyles.background}>
+        <div style={{ textAlign: 'center', color: 'gray' }}>
           <h1>{data.site.siteMetadata.author}</h1>
-          <p>Software Developer</p>
+          <h4 style={{padding:'0px 100px', color: 'gray' }}>Actively searching for Computer Programming, Software Development, Software Engineer, Jr. Developer position. I have recently graduated from Helio Training and have received a Certificate of Proficiency in Full Stack Web Development. I have built projects using the following: Javascript, React, MongoDB, GraphQL, REST, GatsbyJS, Monk, HTML, CSS. I am looking for a full time position in industry. </h4>
           <div>
-          <a style={{ color: 'black', }} href='https://www.linkedin.com/in/prestondonohoo/' target='_blank' > <Linkedin style={{width: 30, height: 30, marginRight: 20, }} /></a>
-          <a style={{ color: 'black', }} href='https://github.com/pdonohoo' target='_blank' ><Github style={{width: 30, height: 30}} />  </a>
+          <a style={{ color: 'white', }} href='https://www.linkedin.com/in/prestondonohoo/' target='_blank' rel="noopener noreferrer" > <Linkedin style={{width: 30, height: 30, marginRight: 20, }} /></a>
+          <a style={{ color: 'white', }} href='https://github.com/pdonohoo' target='_blank'  rel="noopener noreferrer" ><Github style={{width: 30, height: 30}} />  </a>
           </div>
 
         </div>
